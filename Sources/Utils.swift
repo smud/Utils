07c565848ff80f847c301b6b34e9c6179969379d
Anton Karandeev <tonygluk@gmail.com>
2017-03-18 15:20:23 +0300

    Fix exception in combinedHash()

--- Sources/Utils.swift
+++ Sources/Utils.swift
@@ -4,9 +4,9 @@ import ScannerUtils
 // See SO post http://stackoverflow.com/questions/1835976/what-is-a-sensible-prime-for-hashcode-calculation
 public func combinedHash(_ firstObject: AnyHashable, _ otherObjects: AnyHashable...) -> Int {
     let prime = 92821
-    var hash = prime + firstObject.hashValue
+    var hash = prime &+ firstObject.hashValue
     for object in otherObjects {
-        hash = hash * prime + object.hashValue
+        hash = hash &* prime &+ object.hashValue
     }
 
     return hash
